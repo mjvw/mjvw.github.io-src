a8f7e3f917eec2ae85e46ad609ed252e1a5ab2be
mjvw <matthijsvanwolferen@fastmail.fm>
2017-02-21 15:22:20 +0100

    Fifth commit.

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,7 @@ SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = 'Europe/Paris'
+TIMEZONE = 'Europe/Amsterdam'
 
 DEFAULT_LANG = u'en'
 
@@ -21,9 +21,9 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
+LINKS = (('Despite Our Differences', 'https://despiteourdifferencesblog.wordpress.com/'),
+         ('LinkedIn', 'https://nl.linkedin.com/in/matthijsvanwolferen'),
+         ('SSRN', 'https://ssrn.com/author=2630544'),
          ('You can modify those links in your config file', '#'),)
 
 # Social widget
